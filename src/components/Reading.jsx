1ae972b801da2d8e103d13ff210c8d310ca65eaa
hdwhitem <hdwhite.m@gmail.com>
2023-01-24 20:18:32 +0000

    remove <p> from Reading.jsx

--- src/components/Reading.jsx
+++ src/components/Reading.jsx
@@ -3,12 +3,10 @@ import { QuizContext } from "../contexts/quiz"
 
 const Reading = () => {
   const [quizState, dispatch] = useContext(QuizContext)
-  const currentQuestion = quizState.questions[quizState.currentQuestionIndex]
   return (
     <div>
-      <div className="question">{currentQuestion.question}</div>
+      <div className="question">Quiz about CSS</div>
       <div className="answers">
-        <p>text reading</p>
       <div onClick={() => dispatch({ type: "START_QUIZ" })} className="next-button">
         Start Quiz
       </div>
